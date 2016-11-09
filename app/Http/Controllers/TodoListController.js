4a84e8910b9e3675129e3966e8b8f46fba17c118
Brit Butler <redline6561@gmail.com>
2016-11-09 12:13:37 -0500

    Add API method to create Todo Lists.

--- app/Http/Controllers/TodoListController.js
+++ app/Http/Controllers/TodoListController.js
@@ -1,9 +1,20 @@
 'use strict'
 
+const TodoList = use("App/Model/TodoList")
+
 class TodoListController {
 
   * example (request, response) {
     response.json({ message: "hello world" })
+  }
+
+  * create (request, response) {
+    let userId = request.param('id')
+    let data = request.only('name')
+    data.user_id = userId
+
+    let todoList = yield TodoList.create(data)
+    response.status(201).json(todoList)
   }
 
 }
