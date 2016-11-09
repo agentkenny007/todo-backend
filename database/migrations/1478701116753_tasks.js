feb39b10b9a1f317c7c3dc76f0d20201293aa4f7
Brit Butler <redline6561@gmail.com>
2016-11-09 09:24:04 -0500

    Add migration for tasks.

---
+++ database/migrations/1478701116753_tasks.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const Schema = use('Schema')
+
+class TasksTableSchema extends Schema {
+
+  up () {
+    this.create('tasks', (table) => {
+      table.increments()
+      table.timestamps()
+      table.string('title')
+      table.boolean('done')
+      table.integer('todo_list_id')
+      table.foreign('todo_list_id').references('todo_lists.id')
+      table.date('due_date')
+    })
+  }
+
+  down () {
+    this.drop('tasks')
+  }
+
+}
+
+module.exports = TasksTableSchema
