3e9f419dfea4d623121490ee818de022249c44df
Ikenna <ikennaugwuh@gmail.com>
2016-11-10 12:30:02 -0500

    Added CRUD methods for todo list controller.

--- app/Http/Controllers/TodoListController.js
+++ app/Http/Controllers/TodoListController.js
@@ -1,29 +1,54 @@
 'use strict'
 
 const TodoList = use("App/Model/TodoList")
+const User = use("App/Model/User")
 
 class TodoListController {
 
-  * example (request, response) {
-    response.json({ message: "hello world" })
-  }
-
-  * create (request, response) {
-    let userId = request.param('id')
-    let data = request.only('name')
+  * create (request, response){
+    let userId = request.param('user_id')
+    if (!yield User.find(userId))
+        return response.status(404).json({ error: "User not found." })
+    const data = request.only('name')
     data.user_id = userId
 
-    let todoList = yield TodoList.create(data)
+    const todoList = yield TodoList.create(data)
     response.status(201).json(todoList)
   }
 
-  * index (request, response) {
-    let userId = request.param('id')
-    let lists = yield TodoList.query().where('user_id', userId)
+  * destroy(request, response){
+    const todoList = yield TodoList.find(request.param('list_id'))
 
-    response.json(lists)
+    yield todoList.delete()
+    response.json(todoList)
   }
 
+  * index (request, response){
+    let userId = request.param('user_id')
+    if (!yield User.find(userId))
+        return response.status(404).json({ error: "User not found." })
+    const lists = yield TodoList.findBy('user_id', userId)
+
+    if (lists) response.json(lists)
+    else response.status(404).json({ error: "No lists found for this user." })
+  }
+
+  * show (request, response){
+    const todoList = yield TodoList.find(request.param('list_id'))
+
+    if (todoList) response.json(todoList)
+    else response.status(404).json({ error: "List not found." })
+  }
+
+  * update(request, response){
+      const todoList = yield TodoList.find(request.param('list_id'))
+
+      if (todoList){
+          todoList.fill(request.only('name'))
+          yield todoList.save()
+          response.json(todoList)
+      } else response.status(404).json({ error: "List not found." })
+  }
 }
 
 module.exports = TodoListController
